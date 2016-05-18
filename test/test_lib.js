91b66ca559a2d9b419517bb856fb07e13ffe27c8
Volkan Ozcelik <me@volkan.io>
2016-05-18 09:13:13 -0700

    added default folders.
    needs some more work before merging;
    
    Readme needs to change for instance.
    
    also a version bump and an npm publish would be nice.

--- test/test_lib.js
+++ test/test_lib.js
@@ -4,7 +4,6 @@ const babil = require( '..' );
 const join = require( 'path' ).join;
 
 babil.initialize( {
-    srcPath: join( __dirname, '../examples/sample-project/src' ),
     rootPath: join( __dirname, '../examples/sample-project' ),
     babelPath: join( __dirname, '../node_modules/babel-cli/bin/babel.js' )
 } );
