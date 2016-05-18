b719e4cb36696a51879d1629c5ae0e4450df283b
Volkan Ozcelik <me@volkan.io>
2016-05-18 07:16:18 -0700

    Minor change to code comments.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*
  *  ____    __    ____  ____  __   
  * (  _ \  /__\  (  _ \(_  _)(  )  
- * )  _ < /(__)\ )  _ < _)(_  )(__ 
+ *  ) _ < /(__)\  ) _ < _)(_  )(__ 
  * (____/(__)(__)(____/(____)(____)
  * 
  * This project is a part of the “Byte-Sized JavaScript” videocast.
  *
- * You can watch it at: https://bit.ly/bytesized
+ * You can watch “Byte-Sized JavaScript” at: https://bit.ly/bytesized
  *
  * MIT Licensed — See LICENSE.md
  *
